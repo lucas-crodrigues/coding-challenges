4ad9c72258b3e8685a49d0e48135ffb44490321d
lucascr <lcrodrigues1995@gmail.com>
2024-03-03 20:24:34 -0300

    Create sockMerchant.js

---
+++ sockMerchant/sockMerchant.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+function sockMerchant(n, ar) {
+    // Write your code here
+    let socks = [...ar].sort((a, b) => a - b);
+    let pairs = [];
+    
+    for (let i = 0; i < socks.length; i++) {
+        if (socks[i] === socks[i + 1]) {
+            pairs.push(socks.splice(i, 2));
+            i -= 1;
+        }    
+    }
+
+    return pairs.length
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const ar = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    const result = sockMerchant(n, ar);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
